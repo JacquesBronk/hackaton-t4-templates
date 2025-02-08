4f2eea1559138b725138095b1534918f7db16ee4
JacquesB <jacquesb@agilebridge.co.za>
2025-02-08 11:16:10 +0200

    master stuff

--- Maker/Program.cs
+++ Maker/Program.cs
@@ -7,8 +7,6 @@ if(!Directory.Exists(CommonPaths.FrontEnd))
     Directory.CreateDirectory(CommonPaths.FrontEnd);
 }
 
-
-
 if(!Directory.Exists(CommonPaths.BackEnd))
 {
     Directory.CreateDirectory(CommonPaths.BackEnd);
