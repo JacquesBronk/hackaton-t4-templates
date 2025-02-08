430ccbdcde83d7485afcd39d2383d4607bbf838e
JacquesB <jacquesb@agilebridge.co.za>
2025-02-08 09:05:29 +0200

    #### Changelog * Common filepath helper

---
+++ Maker/Docker/DockerFileMakerThingy.cs
@@ -0,0 +1,39 @@
+﻿namespace Maker.Docker;
+
+public class DockerFileMakerThingy
+{
+    //Dotnet Dockerfile Maker
+    public string MakeDotnetDockerfile(string projectName, string projectPath, string dockerfilePath)
+    {
+        return $@"
+            FROM mcr.microsoft.com/dotnet/sdk:8.0 AS build
+            WORKDIR /source
+
+            COPY {projectPath}/*.csproj .
+            RUN dotnet restore
+
+            COPY {projectPath} .
+            RUN dotnet publish -c release -o /app --no-restore
+
+            FROM mcr.microsoft.com/dotnet/aspnet:5.0
+            WORKDIR /app
+            COPY --from=build /app .
+
+            ENTRYPOINT [""dotnet"", ""{projectName}.dll""]
+        ";
+    }
+    
+    //Nginx Dockerfile Maker
+    public string MakeNginxDockerfile(string nginxConfigPath, string nginxConfigFilePath)
+    {
+        return $@"
+            FROM nginx:latest
+            COPY {nginxConfigPath} /etc/nginx/nginx.conf
+            COPY {nginxConfigFilePath} /etc/nginx/conf.d/default.conf
+        ";
+    }
+    
+    
+    //Compose Dockerfile Maker
+    
+}
