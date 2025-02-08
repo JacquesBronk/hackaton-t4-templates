bb8e5c80b9db16b2412a1a1c5008402b12195473
Wayne Meyer <waynem@agilebridge.co.za>
2025-02-08 10:23:23 +0200

    Html generation working

--- Maker/Program.cs
+++ Maker/Program.cs
@@ -1,3 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System.Text.Json;
 
 Console.WriteLine("Let's make a base");
+
+
+string basePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Definition.json");
+
+Console.WriteLine("Base path: " + basePath);
+
+var jsonFilePath = basePath;
+string jsonContent = File.ReadAllText(jsonFilePath);
+var data = JsonSerializer.Deserialize<JsonDocument>(jsonContent);
